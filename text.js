9d4bbe2d48eacfb5b3a1a50abda981e1e835eeef
yym <you@example.com>
2018-11-15 22:44:52 +0800

    1

--- text.js
+++ text.js
@@ -1,3 +1,6 @@
-/*var hello = "hello, "
-console.log(hello.concat("Kevin","have a nice day."))*/
-'Blue Whale'.includes('blue')
+function dig(obj, target)
+/*给定任意一个对象作为函数的第一个参数，函数的第二个参数为该对象中的某个键名，
+通过这个键名可以直接输出对应的键值，不管对象里面的层级有多深。*/
+{
+    
+}
