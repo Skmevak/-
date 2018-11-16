2e5eaf3f4304027053330b54e94c9b39cd83d458
yym <you@example.com>
2018-11-16 21:55:19 +0800

    3

--- text.js
+++ text.js
@@ -2,5 +2,13 @@ function dig(obj, target)
 /*给定任意一个对象作为函数的第一个参数，函数的第二个参数为该对象中的某个键名，
 通过这个键名可以直接输出对应的键值，不管对象里面的层级有多深。*/
 {
-    
+    for(var name in obj){
+        if(name == target)console.log(obj[name]);
+        else{
+            for(var key in name){
+                if(key == target)console.log(name[key]);
+                else dig(key,target);
+            }
+        }
+    }
 }
